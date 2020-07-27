ce159a5224c09a4ae520eec1f1f48b7e04492bdc
mahesh.gamage@nexa.com.au <mahesh.gamage@nexa.com.au>
2020-07-27 18:07:46 +1000

    Singleton pattern

---
+++ UnitTestDesignPatterns/SingletonTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Singleton;
+
+namespace UnitTestDesignPatterns
+{
+    [TestClass]
+    public class SingletonTests
+    {
+        [TestMethod]
+        public void Singleton_CheckIfDifferent_Objects()
+        {
+            // Arrange
+            var s1 = Singleton.Singleton.GetInstance();
+            var s2 = Singleton.Singleton.GetInstance();
+
+            // Assert
+            Assert.AreSame(s1, s2);
+        }
+    }
+}
